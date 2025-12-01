43198b8d0a3564bb0c713a820785e342cb59ba19
DepiprojectGroup <depiproject7@gmail.com>
2025-12-01 21:38:55 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -223,3 +223,30 @@ max_size     = 2
 min_size     = 1
 }
 }
+
+# iam_role
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "eks.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role" "eks_node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+    }]
+  })
+}
