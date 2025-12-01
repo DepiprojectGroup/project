7aa52c49be6d023f486c5da4bfee421fe96b3323
DepiprojectGroup <depiproject7@gmail.com>
2025-12-02 01:46:18 +0200

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,12 +3,14 @@ output "vpc_id" {
   value       = aws_vpc.main.id
 }
 
-output "public_subnet_ids" {
-  value = [aws_subnet.public.id, aws_subnet.public_2.id]
+output "public_subnet_id" {
+  description = "Public subnet ID"
+  value       = aws_subnet.public.id
 }
 
-output "private_subnet_ids" {
-  value = [aws_subnet.private.id, aws_subnet.private_2.id]
+output "private_subnet_id" {
+  description = "Private subnet ID"
+  value       = aws_subnet.private.id
 }
 
 output "nexus_public_ip" {
