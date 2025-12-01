f9426234dde4d63d7da1717c2cc64400b1c16a67
DepiprojectGroup <depiproject7@gmail.com>
2025-12-01 20:23:28 +0200

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -16,11 +16,11 @@ data "aws_ami" "amazon_linux_2" {
 
 # EC2 Instance for Nexus
 resource "aws_instance" "nexus" {
-  ami                    = ami-0f50f13aefb6c0a5d
+  ami                    = data.aws_ami.amazon_linux_2.id  # صح: استخدم data source
   instance_type          = var.nexus_instance_type
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.nexus.id]
-  key_name               = "hoss" 
+  key_name               = "hoss"  # تأكد إن key "hoss" موجود في AWS
 
   root_block_device {
     volume_size = 8   
