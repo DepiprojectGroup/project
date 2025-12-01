47f470f85272447d4448a6a6b5be4d5f1996be9a
DepiprojectGroup <depiproject7@gmail.com>
2025-12-02 01:45:44 +0200

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -76,7 +76,7 @@ resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "${var.project_name}-node-group"
   node_role_arn   = aws_iam_role.eks_nodes.arn
-  subnet_ids      = [aws_subnet.private_1.id, aws_subnet.private_2.id]
+  subnet_ids      = [aws_subnet.private.id, aws_subnet.private_2.id]
 
   scaling_config {
     desired_size = var.eks_desired_capacity
