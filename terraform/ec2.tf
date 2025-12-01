ac4decb5adf68c48cc95bea81db3bb22eb84f25a
DepiprojectGroup <depiproject7@gmail.com>
2025-12-01 21:01:17 +0200

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -14,14 +14,13 @@ data "aws_ami" "amazon_linux_2" {
   }
 }
 
-# EC2 Instance for Nexus
 resource "aws_instance" "nexus" {
-  ami                    = "ami-0f50f13aefb6c0a5d"
+  ami                    = data.aws_ami.amazon_linux_2.id  # استخدم data source
   instance_type          = var.nexus_instance_type
   subnet_id              = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.nexus.id]
-  key_name               = "" 
-
+  key_name               = ""
+  
   root_block_device {
     volume_size = 8   
     volume_type = "gp2"
